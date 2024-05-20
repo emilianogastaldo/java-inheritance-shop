14ea8c53ca26db18efb223ca7e79b5608f6406bf
emilianogastaldo <emiliano0303@gmail.com>
2024-05-20 14:40:02 +0200

    Smartphone

--- src/javashop/Smartphone.java
+++ src/javashop/Smartphone.java
@@ -1,4 +1,37 @@
 package javashop;
 
-public class Smartphone {
+import java.math.BigDecimal;
+import java.util.Random;
+
+public class Smartphone extends Prodotto{
+
+    private int imei;
+    private int memoria;
+
+    Smartphone(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva, int imei, int memoria){
+        super(nome, descrizione, prezzo, iva);
+        this.imei = imeiGenerator();
+        this.memoria = memoria;
+    };
+
+    private int imeiGenerator(){
+        Random random = new Random();
+        return random.nextInt(10000000);
+    }
+
+    public int getImei() {
+        return imei;
+    }
+
+    public void setImei(int imei) {
+        this.imei = imei;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
 }
