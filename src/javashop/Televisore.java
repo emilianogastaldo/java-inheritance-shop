0835eb175edb01a224763c3b6bef327730aa7086
emilianogastaldo <emiliano0303@gmail.com>
2024-05-20 14:46:47 +0200

    Televisore

--- src/javashop/Televisore.java
+++ src/javashop/Televisore.java
@@ -1,4 +1,29 @@
 package javashop;
 
-public class Televisore {
+import java.math.BigDecimal;
+
+public class Televisore extends Prodotto{
+    private double pollici;
+    private boolean isSmart;
+    public Televisore(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva, double pollici, boolean isSmart) {
+        super(nome, descrizione, prezzo, iva);
+        this.pollici = pollici;
+        this.isSmart = isSmart;
+    }
+
+    public double getPollici() {
+        return pollici;
+    }
+
+    public void setPollici(double pollici) {
+        this.pollici = pollici;
+    }
+
+    public boolean isSmart() {
+        return isSmart;
+    }
+
+    public void setSmart(boolean smart) {
+        isSmart = smart;
+    }
 }
